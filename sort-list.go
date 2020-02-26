1bbe23a00f7df3c1325e29cd4d743af3ed4036c3
bitsnaps <bitsnaps@yahoo.fr>
2020-02-26 20:03:36 +0100

    A suggestion to sort the list using this script
    This is file is just a suggestion, so if a GoLang developer can makes it works for this repo, I think this gonna be a great way to sort the list by the number of stars of each repo.

---
+++ sort-list.go
@@ -0,0 +1,15 @@
+/*
+This guy created a script to sort awesome lists by the number of stars in each repo:
+https://github.com/kvnxiao/sort-awesome-lists
+I'm not sure if it works in this repo, but if someone good with GoLang can do it here.
+*/
+
+package main
+
+import (
+    "os"
+)
+
+func main() {
+    fmt.Println("Sorting...")
+}
